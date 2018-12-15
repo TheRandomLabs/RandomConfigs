d0036d764d981f129ecb8bed48d89c3aa1d3a3b1
TheRandomLabs <therandomlabsinc@gmail.com>
2018-12-15 11:19:39 +1100

    Minor change

--- src/main/java/com/therandomlabs/randomconfigs/api/event/WorldEvent.java
+++ src/main/java/com/therandomlabs/randomconfigs/api/event/WorldEvent.java
@@ -4,10 +4,6 @@ import net.fabricmc.fabric.util.HandlerList;
 import net.minecraft.server.world.ServerWorld;
 
 public final class WorldEvent {
-	public static final HandlerList<Initialize> INITIALIZE = new HandlerList<>(Initialize.class);
-	public static final HandlerList<CreateSpawnPosition> CREATE_SPAWN_POSITION =
-			new HandlerList<>(CreateSpawnPosition.class);
-
 	@FunctionalInterface
 	public interface Initialize {
 		void onInitialize(ServerWorld world);
@@ -17,4 +13,8 @@ public final class WorldEvent {
 	public interface CreateSpawnPosition {
 		void onCreateSpawnPosition(ServerWorld world);
 	}
+
+	public static final HandlerList<Initialize> INITIALIZE = new HandlerList<>(Initialize.class);
+	public static final HandlerList<CreateSpawnPosition> CREATE_SPAWN_POSITION =
+			new HandlerList<>(CreateSpawnPosition.class);
 }
