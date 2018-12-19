9aea9815fe83be04bcd194dbb21947943130ab31
TheRandomLabs <therandomlabsinc@gmail.com>
2018-12-20 10:47:14 +1100

    Fix world load crash

--- src/main/java/com/therandomlabs/randomconfigs/gamerules/RCGameRules.java
+++ src/main/java/com/therandomlabs/randomconfigs/gamerules/RCGameRules.java
@@ -1,14 +1,17 @@
 package com.therandomlabs.randomconfigs.gamerules;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import com.therandomlabs.randomconfigs.RandomConfigs;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.GameRules;
 
 public class RCGameRules extends GameRules {
-	private static final Field RULES = RandomConfigs.findField(GameRules.class, "rules", "b");
+	private static final Field RULES =
+			RandomConfigs.findField(GameRules.class, "rules", "field_9196");
 
 	private final Set<String> forced;
 
